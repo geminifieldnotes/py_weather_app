8e4abdac9165c3b48b9a15fc2cac316c224c519a
hxueting <77209297+Abao01@users.noreply.github.com>
2022-04-05 10:22:09 -0500

    Create dbcm.py
    initial commit for dbcm file

---
+++ dbcm.py
@@ -0,0 +1,34 @@
+import sqlite3
+
+class DBCM():
+  """Create a context manager DBCM class to manage the database connections"""
+  def __init__(self, db_name):
+    """Initializes the variables."""
+    try:
+      self.db_name = db_name
+      self.conn = None
+      self.curs = None
+    except Exception as error:
+      print("Error: DBCM: Init: ", error)
+
+  def __enter__(self):
+    """Opens a connection and cursor."""
+    try:
+      self.conn = sqlite3.connect(self.db_name)
+      print("Opened database successfully.")
+    except Exception as error:
+      print("Error opening DB:", error)
+    try:
+      self.curs = self.conn.cursor()
+      return self.curs
+    except Exception as error:
+      print("Error database Enter: ", error)
+
+  def __exit__(self, exc_type, exc_value, exc_trace):
+    """Commits changes and closes cursor and connection."""
+    try:
+      self.conn.commit()
+      self.curs.close()
+      self.conn.close()
+    except Exception as error:
+      print("Error database Exit: ", error)
