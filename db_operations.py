106723f82001dc65106e77173ae44b279b867c38
hxueting <77209297+Abao01@users.noreply.github.com>
2022-04-05 11:12:44 -0500

    Update db_operations.py
    Modify db_operations file

--- db_operations.py
+++ db_operations.py
@@ -1,48 +1,55 @@
 import sqlite3
+from dbcm import DBCM
 
 class DBOperations:
-  def __init__(self, db_file):
-    try:
-        self.conn = sqlite3.connect("weather.sqlite")
-        print("Opened database successfully.")
-    except Exception as e:
-            print("Error opening DB:", e)
-
 
   def initialize_db(self):
-    try:
-      self.c = self.conn.cursor()
-      self.c.execute("""create table IF NOT EXISTS weatherdata
-                        (id integer primary key autoincrement not null,
-                        sample_date text not null,
-                        location text not null,
-                        min_temp real not null,
-                        max_temp real not null,
-                        avg_temp real not null);""")
-      self.conn.commit()
-      print("Table created successfully.")
-    except Exception as e:
-      print("Error creating table:", e)
+    with DBCM("weather.sqlite") as cur:
+      try:
+        cur.execute("""create table if not exists weatherdata
+                        ( id integer primary key autoincrement not null,
+                          sample_date text not null,
+                          location text not null,
+                          max_temp real not null,
+                          min_temp real not null,
+                          avg_temp real not null);""")
+        print("Table created successfully.")
+      except Exception as e:
+            print("Error creating table:", e)
 
-  def fetch_data(self,dictionary):
+  def save_date(self,dictionary):
+      try:
+        sql = """insert into weatherdata (sample_date,location,max_temp,min_temp,avg_temp)
+                values (?,?,?,?,?)"""
+        try:
+          for dates,datas in dictionary.items():
+            data = []
+            data.append(dates)
+            data.append("Winnipeg, MB")
+            for key, value in datas.items():
+                data.append(value)
+            with DBCM("weather.sqlite") as cur:
+              cur.execute(sql, data)
+          print("Added weatherdata successfully.")
+        except Exception as e:
+          print("Error: ",e)
+      except Exception as e:
+        print("Error inserting weatherdata.", e)
 
-    sql = """insert into samples (date,location,max_temp,min_temp,avg_temp)
-                  values (?,?,?,?,?)"""
+  def purge_data(self):
+    with DBCM("weather.sqlite") as cur:
+      sql= """delete from  weatherdata"""
+      for row in cur.execute(sql):
+        print(row)
+      print(" Purge all the data from the DB. ")
 
-    for dates,datas in dictionary.items():
-      self.data = []
-      self.data.append(dates)
-      self.data.append("Winnipeg")
-      for key, value in datas.items():
-        self.data.append(value)
-        self.c.execute(sql, self.data)
-        self.conn.commit()
-      print("Added sample successfully.")
+weather_dict = {
+            "2018-06-01": {"Max": 12.0, "Min": 5.6, "Mean": 7.1},
+            "2018-06-02": {"Max": 22.2, "Min": 11.1, "Mean": 15.5},
+            "2018-06-03": {"Max": 31.3, "Min": 29.9, "Mean": 30.0}
+        }
 
-  def save_data(self):
-    try:
-      for row in self.c.execute("select * from weatherdata"):
-          print(row)
-    except Exception as e:
-        print("Error fetching samples.", e)
-    self.conn.close()
+db = DBOperations()
+db.initialize_db()
+db.save_date(weather_dict)
+db.purge_data()
