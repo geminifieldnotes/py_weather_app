1e7faec7aa59631fa025b72286b10b3debb100ae
hxueting <77209297+Abao01@users.noreply.github.com>
2022-04-20 09:34:21 -0500

    Update plot_operations.py
    Additional Requirements

--- plot_operations.py
+++ plot_operations.py
@@ -1,46 +1,58 @@
 """Handle the creation of box and line plots"""
+import logging
 import matplotlib.pyplot as plt
-import numpy as np
 from db_operations import DBOperations
 
 class PlotOperations:
     """Class for reveiving weather data and ploting"""
     def initialize(self):
-      """Initialize the class"""
+        """Initialize the class"""
 
-    def box_plot(self, weather_data):
+    def box_plot(self, weather_datas):
         """Plots  of mean temperatures in a date range (year to year)"""
         data = []
-        for month in weather_data:
-            data.append(weather_data[month])
+        try:
+            for month in weather_datas:
+                data.append(weather_datas[month])
+        except Exception as error:
+            logging.warning("Error: Organizing line box data: %s",error)
 
-        plt.boxplot(data)
-        plt.title(f'Monthly Temperature Distribution for:{start_year} to {end_year}')
-        plt.ylabel('Temperature (Celsius)')
-        plt.xlabel('Month')
-        plt.show()
+        try:
+            plt.boxplot(data)
+            plt.title(f'Monthly Temperature Distribution for:{START_YEAR} to {END_YEAR}')
+            plt.ylabel('Temperature (Celsius)')
+            plt.xlabel('Month')
+            plt.show()
+        except Exception as error:
+            logging.warning("Error: Create box plot: %s",error)
 
-    def line_plot(self, weather_data):
+
+    def line_plot(self, weather_datas):
         """ plot which shows the mean daily temp of a particular month and year"""
         data = []
         dates = []
-        for day in weather_data:
-            dates.append(day)
-            data.append(weather_data[day])
+        try:
+            for day in weather_datas:
+                dates.append(day)
+                data.append(weather_datas[day])
+        except Exception as error:
+            logging.warning("Error: Organizing line plot data: %s",error)
+        try:
+            plt.plot(dates, data)
+            plt.title('Daily Avg Temperatures')
+            plt.ylabel('Avg Daily Temp')
+            plt.xlabel('Day of Month')
+            plt.xticks(rotation=45)
+            plt.show()
+        except Exception as error:
+            logging.warning("Error: Create line plot: %s",error)
 
-        plt.plot(dates, data)
-        plt.title('Daily Avg Temperatures')
-        plt.ylabel('Avg Daily Temp')
-        plt.xlabel('Day of Month')
-        plt.xticks(rotation=45)
-        plt.show()
-
-start_year = '2000'
-end_year = '2022'
-weather_data = DBOperations().fetch_data(start_year, end_year)
+START_YEAR = '2000'
+END_YEAR = '2022'
+weather_data = DBOperations().fetch_data(START_YEAR, END_YEAR)
 PlotOperations().box_plot(weather_data)
 
-year = '2020'
-month = '03'
-weather_data = DBOperations().fetch_data(year, month)
+YEAR = '2020'
+MONTH= '06'
+weather_data = DBOperations().fetch_data(YEAR, MONTH)
 PlotOperations().line_plot(weather_data)
