b73bada27f74cd316de0a2112f79dbeeea241231
hxueting <77209297+Abao01@users.noreply.github.com>
2022-04-20 09:37:37 -0500

    Update dbcm.py
    Additional Requirements

--- dbcm.py
+++ dbcm.py
@@ -1,35 +1,36 @@
 """This model is Database Context Manager"""
 import sqlite3
+import logging
 
 class DBCM():
-  """DBCM class to manage the database connections"""
-  def __init__(self, db_name):
-    """Initializes the variables."""
-    try:
-      self.db_name = db_name
-      self.conn = None
-      self.cur = None
-    except Exception as error:
-      print("Error Init: ", error)
+    """DBCM class to manage the database connections"""
+    def __init__(self, db_name):
+        """Initializes the variables."""
+        try:
+            self.db_name = db_name
+            self.conn = None
+            self.cur = None
+        except Exception as error:
+            logging.warning("Error DBCM Init: %s", error)
 
-  def __enter__(self):
-    """Opens a connection and cursor."""
-    try:
-      self.conn = sqlite3.connect(self.db_name)
-      print("Opened database successfully.")
-    except Exception as error:
-      print("Error opening DB:", error)
-    try:
-      self.cur = self.conn.cursor()
-      return self.cur
-    except Exception as error:
-      print("Error database Enter: ", error)
+    def __enter__(self):
+        """Opens a connection and cursor."""
+        try:
+            self.conn = sqlite3.connect(self.db_name)
+            print("Opened database successfully.")
+        except Exception as error:
+            logging.warning("Error opening DB: %s", error)
+        try:
+            self.cur = self.conn.cursor()
+            return self.cur
+        except Exception as error:
+            logging.warning("Error database Enter: %s", error)
 
-  def __exit__(self, exc_type, exc_value, exc_trace):
-    """Commits changes and closes cursor and connection."""
-    try:
-      self.conn.commit()
-      self.cur.close()
-      self.conn.close()
-    except Exception as error:
-      print("Error database Exit: ", error)
+    def __exit__(self, exc_type, exc_value, exc_trace):
+        """Commits changes and closes cursor and connection."""
+        try:
+            self.conn.commit()
+            self.cur.close()
+            self.conn.close()
+        except Exception as error:
+            logging.warning("Error database Exit: %s", error)
